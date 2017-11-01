1d0c18cc30435d9f244e25e5a01ff01c252e35d4
Unknown <castiel.andrew@gmail.com>
2017-11-01 14:27:09 -0700

    First Menu

--- Li_Leung/app/src/main/java/ca/bcit/ass3/li_leung/MainActivity.java
+++ Li_Leung/app/src/main/java/ca/bcit/ass3/li_leung/MainActivity.java
@@ -1,13 +1,88 @@
 package ca.bcit.ass3.li_leung;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list_event = (ListView) findViewById(R.id.list_events);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu. This adds items to the app bar.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.create_event:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    private String[] getEvents() {
+        SQLiteOpenHelper helper = new PotluckDbHelper(this);
+        String[] events = null;
+        try {
+            db = helper.getReadableDatabase();
+            cursor = db.rawQuery("select  from EVENT_MASTER", null);
+
+            int count = cursor.getCount();
+            events = new String[count];
+
+            if (cursor.moveToFirst()) {
+                int index = 0;
+                do {
+                    events[index++] = cursor.getString(0);
+                } while (cursor.moveToNext());
+            }
+        }catch (SQLiteException sqlex) {
+            String msg = "[MainActivity / getContinents] DB unavailable";
+            msg += "\n\n" + sqlex.toString();
+
+            Toast t = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+            t.show();
+        }
+
+        return events;
+    }
+
+
 }
